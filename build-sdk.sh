49fc91c3e0631b18879c812f81b3e304700e0f2a
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2020-10-25 12:19:26 +0100

    Change naming of output tarball
    Now it is based on output of git describe. So, it will include tags,
    commits and dirty indicator

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 # builds SDK package and exports to host system
 # this is meant to run in container only
-tar -zcvf /home/admin/workspace/${TARGET}.tar.gz ${SDK_ROOT}
-sudo cp -pr /home/admin/workspace/${TARGET}.tar.gz /mnt/outdir/
+# Usage:
+#   build-sdk.sh IMAGE IMAGE-VERSION
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 IMAGE IMAGE-VERSION"
+	exit 1
+fi
+
+IMAGE=$1
+IMAGE_VERSION=$2
+
+tar -zcvf /home/admin/workspace/${IMAGE}-${IMAGE_VERSION}.tar.gz ${SDK_ROOT}
+sudo cp -pr /home/admin/workspace/${IMAGE}-${IMAGE_VERSION}.tar.gz /mnt/outdir/
